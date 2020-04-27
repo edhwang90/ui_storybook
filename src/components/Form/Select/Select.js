ac80843fc5955e88cc686caef81f5d7e955cfbcd
EDH <ambp@Apples-MacBook-Pro.local>
2020-04-27 11:43:04 -0400

    line-height bug
    line-height bug

--- src/components/Form/Select/Select.js
+++ src/components/Form/Select/Select.js
@@ -113,24 +113,23 @@ export const Select = (props) => {
 
   return (
     <div className={ isMultiSelect ? `multi-select-container ${selected.length > 0 ? 'show-selected' : ''}`: 'select-container'}>
-      <div className="select-btn-container">
-        <div className={`select-btn${ isOpen ? ' list-open' : '' }`}
-             onClick={toggleSelect}
-             ref={menuRef}>
-          <div className="select-label">
-            {uiLabel()}
-          </div>
+      <div className={`select-btn${ isOpen ? ' list-open' : '' }`}
+            onClick={toggleSelect}
+            ref={menuRef}>
+        <div className="select-display">
+          {uiLabel()}
+        </div>
 
-          <div className="select-actions">
-            {
-              !required && uiClear()
-            }
-            <button className="select-chevron">
-              <svg viewBox="0 0 20 20" aria-hidden="true" fill="#4d4d4d" aria-hidden="true" focusable="false"><path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path></svg>
-            </button>
-          </div>
+        <div className="select-actions">
+          {
+            !required && uiClear()
+          }
+          <button className="select-chevron">
+            <svg viewBox="0 0 20 20" aria-hidden="true" aria-hidden="true" focusable="false"><path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path></svg>
+          </button>
         </div>
       </div>
+
       {
         isOpen &&
         <ul className="select-list">

a0621dbb150e93c5e9a0d98b8bc2dccff6e8831e
EDH <ambp@Apples-MacBook-Pro.local>
2020-04-27 20:33:11 -0400

    stories
    stories clean up

--- src/stories/Form.stories.js
+++ src/stories/Form.stories.js
@@ -15,28 +15,36 @@ export default {
   }
 };
 
-  const options2 = [
-    { name: 'hello', id: 1 },
-    { name: 'goodbye', id: 2 },
-    { name: 'aufwiedersehen', id: 3}
-  ]
+const options = [
+  'Lorem ipsum',
+  'Sed ut perspiciatis unde omnis iste natus',
+  'voluptatem',
+  'laudantium',
+  'quasi'
+];
+
+const options2 = [
+  { name: 'hello', id: 1 },
+  { name: 'goodbye', id: 2 },
+  { name: 'aufwiedersehen', id: 3}
+];
 
 export const Checkboxes = () => (
   <div>
-    <Checkbox toggleProp={true}
-              label="Checkbox"
+    <Checkbox toggleProp={false}
+              label="Default"
               id={1}
               handleToggle={action('toggle')}>
     </Checkbox>
 
-    <Checkbox toggleProp={false}
-            label="Checkbox"
+    <Checkbox toggleProp={true}
+            label="Prefilled"
             id={2}
             handleToggle={action('toggle')}>
     </Checkbox>
 
     <Checkbox toggleProp={false}
-            label="Checkbox"
+            label="Disabled"
             id={3}
             disabled
             handleToggle={action('toggle')}>
@@ -49,7 +57,7 @@ export const Inputs = () => (
     <div className="row">
       <div className="col">
         <div className="form-group">
-          <label className="form-label">Username</label>
+          <label className="form-label">Text</label>
           <FormField validateOnChange onChange={action('change')}>
             <input className="form-input" type="text" placeholder="Username"></input>
           </FormField>
@@ -61,7 +69,8 @@ export const Inputs = () => (
             <input className="form-input" type="password" placeholder="Password"></input>
           </FormField>
         </div>
-
+      </div>
+      <div className="col">
         <div className="form-group">
           <label className="form-label">Email</label>
           <FormField validateOnChange onChange={action('change')}>
@@ -70,30 +79,11 @@ export const Inputs = () => (
         </div>
 
         <div className="form-group">
-          <label className="form-label">Bio</label>
+          <label className="form-label">Textarea</label>
           <FormField validateOnChange onChange={action('change')}>
             <textarea minLength="15" className="form-input" placeholder="Bio..."></textarea>
           </FormField>
         </div>
-      </div>
-      <div className="col">
-        <div className="form-group">
-          <label className="form-label">Select</label>
-          <Select options={options2}
-                  attr="name"
-                  label="Select..."
-                  onClick={action('select')}></Select>
-        </div>
-
-        <div className="form-group">
-          <label className="form-label">another</label>
-          <Select options={options2}
-                  attr="name"
-                  isMultiSelect
-                  value={ [{name: 'hello', id: 5}, {name: 'goodbye', id: 9}]}
-                  label="Select..."
-                  onClick={action('change')}></Select>
-        </div> 
       </div>
     </div>
   </div>
@@ -103,14 +93,14 @@ export const InputGroups = () => (
   <div className="row">
     <div className="col">
       <div className="form-group">
-        <label className="form-label">Twitter</label>
+        <label className="form-label">Prepend Input</label>
         <FormField validateOnChange onChange={action('change')}>
-          <span className="form-input-prepend">@</span>
+          <span className="form-input-prepend">💯</span>
           <input className="form-input" type="text" placeholder="Twitter"></input>
         </FormField>
       </div>
       <div className="form-group">
-        <label className="form-label">Description</label>
+        <label className="form-label">Prepend Textarea</label>
         <FormField validateOnChange onChange={action('change')}>
           <span className="form-input-prepend">👍</span>
           <textarea className="form-input" placeholder="Type here..."></textarea>
@@ -119,21 +109,68 @@ export const InputGroups = () => (
     </div>
     <div className="col">
       <div className="form-group">
-        <label className="form-label">Twitter</label>
+        <label className="form-label">Append</label>
         <FormField validateOnChange onChange={action('change')}>
           <input className="form-input" type="text" placeholder="Twitter"></input>
-          <span className="form-input-append">@</span>
+          <span className="form-input-append">😀</span>
         </FormField>
       </div>
 
       <div className="form-group">
-        <label className="form-label">Username</label>
+        <label className="form-label">Append Button</label>
         <FormField validateOnChange onChange={e => setTestVal2(e)}>
           <input minLength="3" className="form-input" type="text" placeholder="Username"></input>
           <Button onClick={action('click')} className="form-input-append is-primary">
             Click!
           </Button>
         </FormField>
+      </div>
+    </div>
+  </div>
+)
+
+export const Dropdowns = () => (
+  <div>
+    <div className="row">
+      <div className="col">
+          <div className="form-group">
+            <label className="form-label">Select Obj</label>
+            <Select options={options2}
+                    attr="name"
+                    label="Select..."
+                    onClick={action('select')}></Select>
+          </div>
+      </div>
+      <div className="col">
+        <div className="form-group">
+          <label className="form-label">Select String</label>
+          <Select options={options}
+                  label="Select..."
+                  onClick={action('change')}></Select>
+        </div> 
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <div className="form-group">
+          <label className="form-label">Multiselect</label>
+          <Select options={options2}
+                  attr="name"
+                  isMultiSelect
+                  label="Select..."
+                  onClick={action('change')}></Select>
+        </div> 
+      </div>
+      <div className="col">
+        <div className="form-group">
+          <label className="form-label">Multiselect Prefilled</label>
+          <Select options={options2}
+                  attr="name"
+                  isMultiSelect
+                  value={ [{name: 'hello', id: 1}]}
+                  label="Select..."
+                  onClick={action('change')}></Select>
+        </div> 
       </div>
     </div>
   </div>

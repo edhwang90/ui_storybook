82e871bcb23920a7dce9bf5d3c73fa1bf082ce2e
EDH <ambp@Apples-MacBook-Pro.local>
2020-04-29 11:09:07 -0400

    formfield
    formfield bug misplaced attr

--- src/components/Form/FormField/FormField.js
+++ src/components/Form/FormField/FormField.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import './FormField.scss';
 
@@ -31,8 +32,7 @@ export const FormField = (props) => {
                 return (
                   React.cloneElement(child, {
                     onChange: handleChange,
-                    onBlur: handleChange,
-                    validateOnChange: validateOnChange
+                    onBlur: handleChange
                   })
                 )
               }
